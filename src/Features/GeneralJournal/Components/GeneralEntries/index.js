8232b70e904556a8b634024b2eaf2630d3347e17
shair1020 <ali99shair@gmail.com>
2022-08-18 00:42:34 +0500

    in progress

--- src/Features/GeneralJournal/Components/GeneralEntries/index.js
+++ src/Features/GeneralJournal/Components/GeneralEntries/index.js
@@ -13,75 +13,110 @@ import Paper from "@material-ui/core/Paper";
 import "../../../FinancialStatement/financialstatement.css";
 
 function GeneralEntries() {
-	const getInnerCompoundEntries = (batchEntry) => {
-		let filterredFromGarbage = [];
-		let generalEntriesArray = [];
-		let validLength = 0;
+  const getInnerCompoundEntries = (batchEntry) => {
+    console.log("entry", batchEntry)
+    let filterredFromGarbage = [];
+    let generalEntriesArray = [];
+    let validLength = 0;
 
-		Object.entries(batchEntry).forEach(([key, value]) => {
-			//this check maintains, if the current entry's key is a number, not createdAt or id
-			//that is why, we can use this
-			if (key == validLength) {
-				validLength++;
-				filterredFromGarbage.push(value);
-			}
-		});
+    Object.entries(batchEntry).forEach(([key, value]) => {
+      //this check maintains, if the current entry's key is a number, not createdAt or id
+      //that is why, we can use this
+      if (key == validLength) {
+        validLength++;
+        filterredFromGarbage.push(value);
+      }
+    });
 
-		filterredFromGarbage.forEach((entry) => {
-			generalEntriesArray.push(<TableCell>{`${entry.creditInfo}`}</TableCell>);
-		});
+    filterredFromGarbage.forEach((entry) => {
+      generalEntriesArray.push(<TableCell>{`${entry.creditInfo}`}</TableCell>);
+    });
 
-		return generalEntriesArray;
-	};
+    return generalEntriesArray;
+  };
 
-	const entriesRed = useRef(null);
-	const { documents, error } = useCollection("generalEntry");
+  const getInnerCompoundEntriesdebit = (batchEntry, type) => {
+    console.log("entry", batchEntry)
+    let filterredFromGarbage = [];
+    let generalEntriesArray = [];
+    let validLength = 0;
 
-	return (
-		<div className="div">
-			<div></div>
-			<div>
-				<h1>General Entries</h1>
-				<TableContainer className="financial-container" component={Paper}>
-					<Table className="financial-tables" sx={{ minWidth: 650 }} aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>
-									<h5>Date</h5>
-								</TableCell>
-								<TableCell align="right">
-									<h5>Description</h5>
-								</TableCell>
-								<TableCell align="right">
-									<h5>Debit</h5>
-								</TableCell>
-								<TableCell align="right">
-									<h5>Credit</h5>
-								</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{documents &&
-								documents.map((entry) => (
-									<TableRow key={entry.id}>
-										<TableCell>{moment(entry.createdAt.toDate().toString()).format("Do MMM YY")}</TableCell>
-										{
-											<div className="flexing-description" ref={entriesRed}>
-												{getInnerCompoundEntries(entry)}
-											</div>
-										}
-										{/* this debit and credit info has to be merged with description info  */}
-										<TableCell>{moment(entry.createdAt.toDate().toString()).format("Do MMM YY")}</TableCell>
-										<TableCell>{moment(entry.createdAt.toDate().toString()).format("Do MMM YY")}</TableCell>
-									</TableRow>
-								))}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</div>
-			<div></div>
-		</div>
-	);
+    Object.entries(batchEntry).forEach(([key, value]) => {
+      //this check maintains, if the current entry's key is a number, not createdAt or id
+      //that is why, we can use this
+      if (key == validLength) {
+        validLength++;
+        filterredFromGarbage.push(value);
+      }
+    });
+
+    filterredFromGarbage.forEach((entry) => {
+      if (type === 'debit') {
+        generalEntriesArray.push(entry.debit ? <div>{`${entry.debit}`}</div> : <div />);
+      }
+      else {
+        generalEntriesArray.push(entry.credit ? <div>{`${entry.credit}`}</div> : <div />);
+      }
+    });
+
+    return generalEntriesArray;
+  };
+
+  const { documents, error } = useCollection("generalEntry");
+
+  return (
+    <div className="div">
+      <div></div>
+      <div>
+        <h1>General Entries</h1>
+        <TableContainer className="financial-container" component={Paper}>
+          <Table className="financial-tables" sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <h5>Date</h5>
+                </TableCell>
+                <TableCell>
+                  <h5>Description</h5>
+                </TableCell>
+                <TableCell>
+                  <h5>Debit</h5>
+                </TableCell>
+                <TableCell>
+                  <h5>Credit</h5>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {documents &&
+                documents.map((entry) => (
+                  <TableRow key={entry.id}>
+                    <TableCell>{moment(entry.createdAt.toDate().toString()).format("Do MMM YY")}</TableCell>
+                    {
+                      <div className="flexing-description">
+                        {getInnerCompoundEntries(entry)}
+                      </div>
+                    }
+                    {/* this debit and credit info has to be merged with description info  */}
+                    {
+                      <TableCell>
+                        {getInnerCompoundEntriesdebit(entry, 'debit')}
+                      </TableCell>
+                    }
+                    {
+                      <TableCell>
+                        {getInnerCompoundEntriesdebit(entry, 'credit')}
+                      </TableCell>
+                    }
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default GeneralEntries;
