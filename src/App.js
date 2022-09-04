77862fe2aee47189353975de01f4169fa7b216d8
mutahhirkhan <mutahhir199@gmail.com>
2022-09-04 22:34:43 +0500

    refact: router code minimized, removed unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import MainApp from './Layout/MainApp/MainApp';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import GeneralEntriesContainer from "./Features/GeneralJournal/index";
+import TrialBalances from "./Features/FinancialStatement/TrialBalances";
+import IncomeStatement from "./Features/FinancialStatement/IncomeStatement";
+import BalanceSheet from "./Features/FinancialStatement/BalanceSheet";
+import GeneralEntries from "./Features/GeneralJournal/Components/GeneralEntries/index";
+import OwnerEquity from "./Features/FinancialStatement/OwnerEquity";
+import Test from "./Features/Test/Test";
 
 function App() {
-  return (
-    <div className="App">
-      <MainApp />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="">
+				<Router>
+					<Header />
+					<Routes>
+						<Route path="/" element={<GeneralEntriesContainer />} />
+						<Route path="/trial-balances" element={<TrialBalances />} />
+						<Route path="/income-statement" element={<IncomeStatement />} />
+						<Route path="/balance-sheet" element={<BalanceSheet />} />
+						<Route path="/general-entries" element={<GeneralEntries />} />
+						<Route path="/owner-equity" element={<OwnerEquity />} />
+						<Route path="/test" element={<Test />} />
+					</Routes>   
+				</Router>
+			</div>
+		</div>
+	);
 }
 
 export default App;
